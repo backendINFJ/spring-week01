5a9dd8cacb63b8ded0cc3e0908fbea9a8e0f40cc
yoonjae <backendINFJ@github.com>
2024-05-20 00:53:30 +0900

    Spring 1주차 반복학습

---
+++ src/main/java/com/sparta/springweek01/SpringWeek01Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springweek01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeek01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeek01Application.class, args);
+	}
+
+}
